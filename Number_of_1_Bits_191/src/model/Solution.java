7e5c87efba206fb9476da204d810c226ce71897d
ViniBelo <vinialvesbelo@outlook.com>
2025-02-27 19:58:56 -0300

    Number of 1 bits

---
+++ Number_of_1_Bits_191/src/model/Solution.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Solution {
+    public int hammingWeight(int n) {
+        int counter = 0;
+        while (n > 0) {
+            if ((n & 1) == 1) counter++;
+            n >>= 1;
+        }
+        return counter;
+    }
+}

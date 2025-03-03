74378f58f38111a9e30f491774187b557c4d199f
ViniBelo <vinialvesbelo@outlook.com>
2025-03-03 16:38:02 -0300

    Counting bits

---
+++ Merge_Two_Sorted_Lists_21/src/Main.java
@@ -0,0 +1,15 @@
+import model.ListNode;
+import model.Solution;
+
+public class Main {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        Solution solution = new Solution();
+        ListNode result = solution.mergeTwoLists(l1, l2);
+        while (result != null) {
+            System.out.println(result.val);
+            result = result.next;
+        }
+    }
+}

84cb8e57e7264a95de0637014b73f26438761ca6
ViniBelo <vinialvesbelo@outlook.com>
2025-06-18 19:55:05 -0300

    191. Number of 1 Bits

---
+++ 191. Number of 1 Bits/src/Main.java
@@ -0,0 +1,9 @@
+import model.Solution;
+
+public class Main {
+    public static void main(String[] args) {
+        var solution = new Solution();
+
+        System.out.println(solution.hammingWeight(11));
+    }
+}

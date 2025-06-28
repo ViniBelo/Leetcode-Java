ab518ff958dd4d238ea979a85696dd65b03a2aca
ViniBelo <vinialvesbelo@outlook.com>
2025-06-28 17:11:18 -0300

    Second try (104. Maximum Depth of Binary Tree)

--- 104. Maximum Depth of Binary Tree/src/model/Solution.java
+++ 104. Maximum Depth of Binary Tree/src/model/Solution.java
@@ -1,17 +1,31 @@
 package model;
 
 public class Solution {
+//    First try
+//    public int maxDepth(TreeNode root) {
+//        return dfs(root, 0);
+//    }
+//
+//    private int dfs(TreeNode node, int sum) {
+//        if (node != null) {
+//            sum++;
+//            int left = dfs(node.left, sum);
+//            int right = dfs(node.right, sum);
+//            sum = Math.max(left, right);
+//        }
+//        return sum;
+//    }
+
+//    Second try
     public int maxDepth(TreeNode root) {
-        return dfs(root, 0);
+        return counterDfs(root, 0);
     }
 
-    private int dfs(TreeNode node, int sum) {
-        if (node != null) {
-            sum++;
-            int left = dfs(node.left, sum);
-            int right = dfs(node.right, sum);
-            sum = Math.max(left, right);
+    private int counterDfs(TreeNode current, Integer depth) {
+        if (current == null) {
+            return depth;
         }
-        return sum;
+        return Math.max(counterDfs(current.left, depth + 1),
+                counterDfs(current.right, depth + 1));
     }
 }

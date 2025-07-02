d5e6055fd6b136ce58a8352b1427c9bf89f5db54
ViniBelo <vinialvesbelo@outlook.com>
2025-07-01 21:29:33 -0300

    125. Valid Palindrome (Second try)

--- 125. Valid Palindrome/src/model/Solution.java
+++ 125. Valid Palindrome/src/model/Solution.java
@@ -1,19 +1,41 @@
 package model;
 
 public class Solution {
-    public boolean isPalindrome(String s) {
-        s = s.replaceAll("[^a-zA-Z0-9]", "")
-                .toLowerCase();
-        if (s.isEmpty()) return true;
-        var left = 0;
-        var rigth = s.length() - 1;
-        var middle = rigth / 2;
+//    First try
+//    public boolean isPalindrome(String s) {
+//        s = s.replaceAll("[^a-zA-Z0-9]", "")
+//                .toLowerCase();
+//        if (s.isEmpty()) return true;
+//        var left = 0;
+//        var rigth = s.length() - 1;
+//        var middle = rigth / 2;
+//
+//        while (left <= middle) {
+//            if (s.charAt(left) != s.charAt(rigth)) return false;
+//            left++;
+//            rigth--;
+//        }
+//        return true;
+//    }
 
-        while (left <= middle) {
-            if (s.charAt(left) != s.charAt(rigth)) return false;
-            left++;
-            rigth--;
+//    Second try
+    public boolean isPalindrome(String s) {
+        var left = 0;
+        var right = s.length() - 1;
+
+        while (left < right) {
+            if (!Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            } else if (!Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            } else if (Character.toLowerCase(s.charAt(left)) == Character.toLowerCase(s.charAt(right))) {
+                left++;
+                right--;
+            } else {
+                return false;
+            }
         }
+
         return true;
     }
 }

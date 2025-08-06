0f52d30ee09ae8be18c5599ff24551ead4900e6f
ViniBelo <vinialvesbelo@outlook.com>
2025-08-06 19:00:49 -0300

    1207. Unique Number of Occurrences - Second try

--- 1207. Unique Number of Occurrences/src/model/Solution.java
+++ 1207. Unique Number of Occurrences/src/model/Solution.java
@@ -4,11 +4,19 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class Solution {
+//    First try
+//    public boolean uniqueOccurrences(int[] arr) {
+//        var map = new HashMap<Integer, Integer>();
+//        for (Integer val: arr) map.put(val, map.getOrDefault(val, 0) + 1);
+//        var set = new HashSet<Integer>();
+//        for (Integer val: map.values()) if (!set.add(val)) return false;
+//        return true;
+//    }
+
+//    Second try, better memory usage, but a little bit slower
     public boolean uniqueOccurrences(int[] arr) {
         var map = new HashMap<Integer, Integer>();
         for (Integer val: arr) map.put(val, map.getOrDefault(val, 0) + 1);
-        var set = new HashSet<Integer>();
-        for (Integer val: map.values()) if (!set.add(val)) return false;
-        return true;
+        return map.size() == map.values().stream().distinct().count();
     }
 }

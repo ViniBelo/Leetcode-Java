835ea8a781dc5945aae39a1b378a72de6186b275
ViniBelo <vinialvesbelo@outlook.com>
2025-09-08 16:52:36 -0300

    58. Length of Last Word - Fastest implementation

--- 58. Length of Last Word/src/model/Solution.java
+++ 58. Length of Last Word/src/model/Solution.java
@@ -1,12 +1,24 @@
 package model;
 
 public class Solution {
+//    First implementation:
+//    public int lengthOfLastWord(String s) {
+//        var words = s.split(" ");
+//        var lastWord = words[words.length - 1];
+//        var counter = 0;
+//        for (int i = 0; i < lastWord.length(); i++) {
+//            counter++;
+//        }
+//        return counter;
+//    }
+
+//    Fastest implementation
     public int lengthOfLastWord(String s) {
-        var words = s.split(" ");
-        var lastWord = words[words.length - 1];
         var counter = 0;
-        for (int i = 0; i < lastWord.length(); i++) {
-            counter++;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                counter++;
+            } else if (counter > 0) return counter;
         }
         return counter;
     }

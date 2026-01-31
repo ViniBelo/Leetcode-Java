eba8438d652bdf6dddedf36f3e0b11bca50c32ab
ViniBelo <vinialvesbelo@outlook.com>
2026-01-31 12:35:48 -0300

    1679. Max Number of K-Sum Pairs - Second attemp

--- 1679. Max Number of K-Sum Pairs/src/model/Solution.java
+++ 1679. Max Number of K-Sum Pairs/src/model/Solution.java
@@ -1,28 +1,52 @@
 package model;
 
+import java.util.Arrays;
+
 public class Solution {
+//    Second attemp
     public int maxOperations(int[] nums, int k) {
         int max = 0;
+        int left = 0;
         int right = nums.length - 1;
-        for (int i = 0; i < right; i++) {
-            if (nums[i] < k) {
-                boolean foundPair = false;
-                for (int j = i + 1; j <= right && !foundPair; j++) {
-                    if (nums[i] + nums[j] == k) {
-                        max++;
-                        foundPair = true;
-                        swap(nums, j, right);
-                        right--;
-                    }
-                }
-            }
+        nums = Arrays.stream(nums)
+                .sorted()
+                .toArray();
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == k) {
+                max++;
+                left++;
+                right--;
+            } else if (sum > k) {
+                right--;
+            } else left++;
         }
         return max;
     }
-
-    private void swap(int[] nums, int left, int right) {
-        var aux = nums[left];
-        nums[left] = nums[right];
-        nums[right] = aux;
-    }
+    
+//    First attemp
+//    public int maxOperations(int[] nums, int k) {
+//        int max = 0;
+//        int right = nums.length - 1;
+//        for (int i = 0; i < right; i++) {
+//            if (nums[i] < k) {
+//                boolean foundPair = false;
+//                for (int j = i + 1; j <= right && !foundPair; j++) {
+//                    if (nums[i] + nums[j] == k) {
+//                        max++;
+//                        foundPair = true;
+//                        swap(nums, j, right);
+//                        right--;
+//                    }
+//                }
+//            }
+//        }
+//        return max;
+//    }
+//
+//    private void swap(int[] nums, int left, int right) {
+//        var aux = nums[left];
+//        nums[left] = nums[right];
+//        nums[right] = aux;
+//    }
 }
